4157cd16ea5b1452a480e9f7a317dc799afbc3f6
Raunak85 <raunakraj838@gmail.com>
2024-06-12 15:19:20 +0530

    CreatingClassAndObject

---
+++ CPP/Funciton/ClassAndObject.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    int a,b=4;
+    A(int a){
+        this->a = a;
+    }
+    void show(){
+        cout<<a+b;
+    }
+};
+int main(){
+    A  a(4);
+    a.show();
+}
