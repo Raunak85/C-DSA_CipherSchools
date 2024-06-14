6f23d1f584169216055bf9310d921cb3b66e7cac
Raunak85 <raunakraj838@gmail.com>
2024-06-14 15:38:48 +0530

    VirtualKeyWordWithOverriding

---
+++ CPP/Funciton/Inheritance/Overriding.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Base {
+    public:
+    virtual void print(){
+        cout<<"Base";
+    }
+};
+class Child : public Base {
+    public:
+    void print(){
+        cout<<"Child";
+    }
+};
+
+int main(){
+    // Child c;
+    // c.print(); -- Child
+    // Base b;
+    // b.print() --- Base;
+    // Child c;
+    // c.Base::print(); -- Base;
+    Child c;
+    Base* ptr=&c;
+    ptr->print();
+
+}
