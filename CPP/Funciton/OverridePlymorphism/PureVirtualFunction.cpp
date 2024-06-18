0e3c05ba57651157ecc75ba4189548b64007c6ea
Raunak85 <raunakraj838@gmail.com>
2024-06-18 15:00:21 +0530

    pureVirtual

---
+++ CPP/Funciton/OverridePlymorphism/PureVirtualFunction.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    virtual void show() = 0;
+};
+class B{
+    public:
+    void show(){
+        cout<<"show function";
+    }
+};
+
+int main(){
+
+}
