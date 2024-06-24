589fd8491fae4ad1464b268f944c6548125b8f41
Raunak85 <raunakraj838@gmail.com>
2024-06-24 15:30:22 +0530

    CountOccurence

--- ArrayProblem/FirstOccurence.cpp
+++ ArrayProblem/FirstOccurence.cpp
@@ -5,7 +5,7 @@ int FirstOccurence(int arr[],int n,int key){
     int start =0;
     int end = n-1;
     int mid,occurence=-1;
-    while(start<end){
+    while(start<=end){
         mid = (start+end)/2;
         if(arr[mid] == key){
             occurence = mid;
@@ -26,20 +26,25 @@ int FirstOccurence(int arr[],int n,int key){
 
 int LastOccurence(int arr[],int n,int key){
     int start=0,end=n-1;
-    int mid,occurence=-1;
-    while (start<end)
+    int occurence=-1;
+    int mid = (start+end)/2;
+    while (start<=end)
     {
-        mid = (start+end)/2;
+        
         if(arr[mid] == key){
             occurence = mid;
+            cout<<occurence;
            start = mid + 1;
         }
-        if(key>arr[mid]){
+        else if(key>arr[mid]){
+             cout<<occurence;
             start = mid+1;
         }
         else if(key<arr[mid]){
+             cout<<occurence;
             end = mid-1;
         }
+        mid = (start+end)/2;
     }
     
      return occurence;
@@ -48,19 +53,19 @@ int LastOccurence(int arr[],int n,int key){
 int main(){
     int arr[] = {2,3,3,3,3,3,3,3,4};
     int count;
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int key = 3;
-    int Firstoccurence = FirstOccurence(arr,n,key);
-    int Lastoccurence = LastOccurence(arr,n,key);
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    int key = 4;
+    // int Firstoccurence = FirstOccurence(arr,n,key);
+    int Lastoccurence = LastOccurence(arr,9,key);
 
-    cout<<Firstoccurence<<" ";
-    cout<<Lastoccurence<<endl;
+    // cout<<Firstoccurence<<" ";
+    // cout<<Lastoccurence<<endl;
 
-    if(Firstoccurence == -1 && Lastoccurence == -1){
-        cout<<"Element NOt found";
-    }
-    else{
-        count = (Lastoccurence-Firstoccurence)+1;
-        cout<<count;
-    }
+    // if(Firstoccurence == -1 && Lastoccurence == -1){
+    //     cout<<"Element NOt found";
+    // }
+    // else{
+    //     count = (Lastoccurence-Firstoccurence)+1;
+    //     cout<<count;
+    // }
 }
