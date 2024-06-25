e826ca66c2e8cfb592d47c08f563fe75bbe9ac6c
Raunak85 <raunakraj838@gmail.com>
2024-06-25 15:39:37 +0530

    BubbleSort

---
+++ BubbleSort/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void BubbleSort(int arr[],int n){
+    for(int i=n-1;i>=0;i--){
+        bool flag = true;
+        for(int j=0;j<=i-1;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag = false;
+            }
+        }
+        if(flag){
+            break;
+        }
+    }
+    
+}
+
+int main(){
+    int arr[] = {4,3,2,7,5,9,8};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    BubbleSort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
