39aa1b676a474153932a45a5844d9d0e33dde50b
Raunak85 <raunakraj838@gmail.com>
2024-07-06 18:27:21 +0530

    FindDistinctElement

--- Array/ArrayDistinct2.cpp
+++ Array/ArrayDistinct2.cpp
@@ -1,10 +1,12 @@
+// find distinct value from the array
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1,1,2,2,2,3,3,4,4};
+    int arr[] = {1,1,2,2,2,3,3,4};
     int size = sizeof(arr)/sizeof(arr[0]);
     int i=0;
     int j= i+1;
+
     while(j<size){
         if(arr[i] != arr[j]){
             arr[++i] = arr[j];
