f496991a736354975583dd57b77eda7ff33394ec
Raunak85 <raunakraj838@gmail.com>
2024-07-06 20:06:44 +0530

    Pair Sum

--- Array/PairSum.cpp
+++ Array/PairSum.cpp
@@ -1,8 +1,11 @@
+// Pair Sum, find the target value in array that sum of element of array is equal to target value or not
+// input {3, 1, 5, 10, 0, 2, 7} , is present (2,7): input {3,  5, 10, 1, 4, 7} is present (5,4);
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[] = {3, 1, 5, 10, 0, 2, 7};
+    int arr[] = {3,  5, 10, 1, 4, 7};
     int target = 9;
     int n = sizeof(arr) / sizeof(arr[0]);
     bool pairExists = false;
