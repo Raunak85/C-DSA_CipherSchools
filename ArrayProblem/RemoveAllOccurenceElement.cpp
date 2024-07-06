843021ac4e0266ee8d2bcd0f95ad14f99c776651
Raunak85 <raunakraj838@gmail.com>
2024-07-06 18:22:13 +0530

    removeOccurence

--- ArrayProblem/RemoveAllOccurenceElement.cpp
+++ ArrayProblem/RemoveAllOccurenceElement.cpp
@@ -1,3 +1,4 @@
+// remove all occurence which given by the key value from the main method like input 2332 , key = 3 then output = 2 2 0 0
 #include<iostream>
 using namespace std;
 
@@ -18,7 +19,7 @@ void RemoveOccurence(int arr[],int n,int key){
 int main(){
     int arr[] = {3,2,2,3};
     int key=3;
-    
+
     int n=sizeof(arr)/sizeof(arr[0]);
 
     RemoveOccurence(arr,n,key);
