bfa472f53d54c54369604182742fffa37acc2611
Raunak85 <raunakraj838@gmail.com>
2024-07-06 18:20:24 +0530

    RemoveAllOccurencece

--- ArrayProblem/RemoveAllOccurenceElement.cpp
+++ ArrayProblem/RemoveAllOccurenceElement.cpp
@@ -18,7 +18,9 @@ void RemoveOccurence(int arr[],int n,int key){
 int main(){
     int arr[] = {3,2,2,3};
     int key=3;
+    
     int n=sizeof(arr)/sizeof(arr[0]);
+
     RemoveOccurence(arr,n,key);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
