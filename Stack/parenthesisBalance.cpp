83837b222e7e1777b666dfc84939e089387cc830
Raunak85 <raunakraj838@gmail.com>
2024-07-06 22:55:50 +0530

    isParenthesisBalanced

--- Stack/parenthesisBalance.cpp
+++ Stack/parenthesisBalance.cpp
@@ -1,58 +1,63 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
 
-bool isBalance(string s){
+bool isBalance(string s)
+{
     stack<char> st;
-    for(int i=0;i<s.size();i++){
-        if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
             st.push(s[i]);
-        } 
-        // else if(s[i] == st.top()){
-        //     st.pop();
-        // }
-        // else{
-        //     break;
-        // }
-        else if(s[i] == ')'){
-            if(!s.empty() && st.top() == '('){
+        }
+        else if (s[i] == ')')
+        {
+            if (!st.empty() && st.top() == '(')
+            {
                 st.pop();
             }
-            else{
-                break;
+            else
+            {
+                return false;
             }
         }
-        else if(s[i] == '}'){
-            if(!s.empty() && st.top() == '{'){
+        else if (s[i] == '}')
+        {
+            if (!st.empty() && st.top() == '{')
+            {
                 st.pop();
             }
-                        else{
-                break;
+            else
+            {
+                return false;
             }
         }
-        else if(s[i] == ']'){
-            if(!s.empty() && st.top() == '['){
+        else if (s[i] == ']')
+        {
+            if (!st.empty() && st.top() == '[')
+            {
                 st.pop();
             }
-            
-        }else{
-            break;
+        }
+        else
+        {
+            return false;
         }
     }
-    if(!st.empty()){
-        // cout<<"656";
-        return false;
-    }
-       cout<<"ll";
-        return true;
+    return st.empty();
     
 }
 
-int main(){
-    string s = "{([])}])";
-    if(isBalance(s)){
-        cout<<"Balanced";
-    }else{
-        cout<<"NOT";
+int main()
+{
+    string s = "{([])}]";
+    if (isBalance(s))
+    {
+        cout << "Balanced";
+    }
+    else
+    {
+        cout << "NOT";
     }
 }
